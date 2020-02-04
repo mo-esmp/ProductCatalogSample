57b358477f62c35d3da2baee5be3aeb45513ff96
Mohsen Esmailpour <mo.esmp@hotmail.com>
2020-02-05 01:02:15 +0330

    Add integration test project.

---
+++ test/IntegrationTests/TestSetup/DbInitializer.cs
@@ -0,0 +1,15 @@
+﻿using Catalog.Api.Infrastructure.Data;
+
+namespace IntegrationTests.TestSetup
+{
+    internal static class DbInitializer
+    {
+        public static void InitializeDb(ProductDbContext context)
+        {
+            foreach (var product in InitialProductList.Products)
+                context.Products.Add(product);
+
+            context.SaveChanges();
+        }
+    }
+}
---
+++ test/IntegrationTests/TestSetup/TestingWebApplicationFactory.cs
@@ -0,0 +1,42 @@
+﻿using Catalog.Api.Infrastructure.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+
+namespace IntegrationTests.TestSetup
+{
+    public class TestingWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<ProductDbContext>));
+                if (descriptor != null)
+                    services.Remove(descriptor);
+
+                services.AddDbContext<ProductDbContext>(options => options.UseInMemoryDatabase("InMemoryDbForTesting"));
+                var sp = services.BuildServiceProvider();
+
+                using var scope = sp.CreateScope();
+                var scopedServices = scope.ServiceProvider;
+                var db = scopedServices.GetRequiredService<ProductDbContext>();
+                var logger = scopedServices.GetRequiredService<ILogger<TestingWebApplicationFactory<TStartup>>>();
+                db.Database.EnsureCreated();
+
+                try
+                {
+                    DbInitializer.InitializeDb(db);
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, "An error occurred seeding the database with test messages. Error: {Message}", ex.Message);
+                }
+            });
+        }
+    }
+}
