727bc5283a55bfbeb37edf4ced35bf6f61b0bfde
Mohsen Esmailpour <mo.esmp@hotmail.com>
2020-02-08 23:15:00 +0330

    Add product status EF migration.

---
+++ src/Catalog.Api/Infrastructure/Data/Migrations/20200208194336_AddStatusToProduct.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Catalog.Api.Infrastructure.Data.Migrations
+{
+    public partial class AddStatusToProduct : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Products",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Products");
+        }
+    }
+}
