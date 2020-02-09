0a22ea8f50ccee155a964489c2420d67d08c204a
Mohsen Esmailpour <mo.esmp@hotmail.com>
2020-02-09 22:03:30 +0330

    Apply database migration at runtime.

---
+++ src/Catalog.Api/Infrastructure/Data/MigrationManager.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Linq;
+
+namespace Catalog.Api.Infrastructure.Data
+{
+    public static class MigrationManager
+    {
+        public static IHost MigrateDatabase(this IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                using var dbContext = scope.ServiceProvider.GetRequiredService<ProductDbContext>();
+                try
+                {
+                    var applied = dbContext.GetService<IHistoryRepository>()
+                        .GetAppliedMigrations()
+                        .Select(m => m.MigrationId);
+
+                    var total = dbContext.GetService<IMigrationsAssembly>()
+                        .Migrations.Select(m => m.Key);
+
+                    var hasPendingMigration = total.Except(applied).Any();
+                    if (hasPendingMigration)
+                        dbContext.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    throw;
+                }
+            }
+
+            return host;
+        }
+    }
+}
--- src/Catalog.Api/Program.cs
+++ src/Catalog.Api/Program.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Catalog.Api.Infrastructure.Data;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Catalog.Api
 {
@@ -13,7 +8,7 @@ namespace Catalog.Api
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().MigrateDatabase().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
